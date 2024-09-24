01675fdbf85d345584478c57a0a39740c60cd20f
Pasan Ekanayake <pasanekanayake@gmail.com>
2024-09-25 02:53:12 +0530

    commit

--- src/database/ProgressBar.java
+++ src/database/ProgressBar.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package database;
 
-/**
- *
- * @author Pasan Ekanayake
- */
 public class ProgressBar extends javax.swing.JFrame {
 
     /**
