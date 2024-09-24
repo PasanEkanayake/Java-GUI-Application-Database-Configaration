e48546b825f13e5a492a6ffc7defbacc2482c8a8
Pasan Ekanayake <pasanekanayake@gmail.com>
2024-09-24 13:12:41 +0530

    commit

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,16 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package database;
-
-
 
 public class Database {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         ProgressBar pb = new ProgressBar();
         pb.setVisible(true);
